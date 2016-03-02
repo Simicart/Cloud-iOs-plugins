77d4a5bcf7a7fe1795456592cf86780afc438d07
axehoang <axe@simicart.com>
2016-03-02 23:41:42 +0700

    update payments

--- CCAvenue/CCAvenueModel.h
+++ CCAvenue/CCAvenueModel.h
@@ -7,6 +7,7 @@
 //
 
 #import <Foundation/Foundation.h>
+#import <SimiCartBundle/SimiOrderModel.h>
 
 #define DidGetRSACCAvenue  @"DidGetRSACCAvenue"
 #define DidUpdateCCAvenuePayment @"DidUpdateCCAvenuePayment"
